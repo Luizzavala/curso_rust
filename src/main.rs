71db7a334f627da75744d59ede8956597b0fc2e9
Luis Miguel Zavala Felix <luisbancaprepa@gmail.com>
2022-07-25 12:18:27 -0600

    Create Var

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    let edad: i8 = 36;
-    println!("Hola soy luis y tengo {} años", edad);
+    // i8 is for numbers positive and negative, but u8 is for only numbers positive.
+    let mut edad : i8 = 36;
+    let name : &str = "Luis";
+
+    edad  = edad + 1;
+
+    println!("Hi, my name is {name} and have {edad} years old.");
 }
