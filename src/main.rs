bbaf4101219da96ed7d1f28a8a40e22b901b5b00
Luis Miguel Zavala Felix <luisbancaprepa@gmail.com>
2022-07-25 17:57:40 -0600

    Loops

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::{io::Stdout, string};
-
 fn main() {
-    println!("insert your Age: ");
-    let mut age: String = String::new();
-    std::io::stdin().read_line(&mut age).unwrap();
+    let number_1 = 123;  
+    let number_2 = 123;
 
-    // parse the age into a integer
-    let age_int : u8 = age.trim().parse().unwrap();
-
-    if age_int >= 18 {
-        println!("your age is {}, enjoi your membership", age_int);
-    } else {
-        println!("your age is {}, you cant pass", age_int);
+    let sum = number_1 + number_2;
+    
+    loop {
+        //mostrar los dos numeros en consola
+        println!("First number: {}, Second number: {},", number_1, number_2);
+        
+        //Obtener del usuario el numero que represnta la suma
+        let mut user_sum = String::new();
+        std::io::stdin().read_line(&mut user_sum).unwrap();
+        let user_sum_int: i32 = user_sum.trim().parse().unwrap();
+        
+        
+        if user_sum_int == sum {
+            println!("that sum its correct:");
+            break;
+        } else {
+            println!("This sum its incorrect: that sum is{}", sum);
+        }
     }
 }
